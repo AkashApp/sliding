44a0d03a055facb0ef524159573e45bed8d94572
akash-borate <27akashborate@gmail.com>
2025-04-27 12:29:22 +0530

    Sliding windows

--- slidingWindow.js
+++ slidingWindow.js
@@ -7,7 +7,7 @@
 // Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
 // Output: [3,3,5,5,6,7]
 
-const maxSlidingWindow = (nums, k) => {
+const maxWindow = (nums, k) => {
     const result = [];
     const n= nums.length;
     for (let i = 0; i < n - k + 1; i++) {
@@ -21,7 +21,26 @@ const maxSlidingWindow = (nums, k) => {
     }
     return result;
 }
+console.log(maxWindow([1,3,-1,-3,5,3,6,7], 3));
+
+
+// Sliding window approach
+
+const maxSlidingWindow = (nums, k) => {
+    const result = [];
+    const queue = [];
+    for (let i = 0; i < nums.length; i++) {
+        while(queue.length > 0 && queue[queue.length - 1] < nums[i]) {
+            queue.pop();
+        }
+        queue.push(nums[i]);
+        if(i >= k - 1) {
+            result.push(queue[0]);
+            if(nums[i - k + 1] === queue[0]) {
+                queue.shift();
+            }
+        }
+    }
+    return result;
+}
 console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3));
-
-
-
